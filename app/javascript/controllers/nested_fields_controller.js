7e47e88c302f516ebfbb21f73299704ac4329c97
Nikita Plyusnin <me@plyusnin.dev>
2025-06-25 12:57:18 +0300

    fix nested form properties bug

--- app/javascript/controllers/nested_fields_controller.js
+++ app/javascript/controllers/nested_fields_controller.js
@@ -2,13 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["template", "container"]
+  static values = { childIndex: String }
 
   connect() {
   }
 
   add(event) {
     event.preventDefault()
-    const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime())
+    const content = this.templateTarget.innerHTML.replaceAll(this.childIndexValue, new Date().getTime())
     this.containerTarget.insertAdjacentHTML('beforeend', content)
   }
   
