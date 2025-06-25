f0bea6571637285c5b73b6c21db9c758d775615b
Nikita Plyusnin <me@plyusnin.dev>
2025-06-25 14:52:02 +0300

    add feature sortable and fix destroy properties list

--- app/javascript/controllers/nested_fields_controller.js
+++ app/javascript/controllers/nested_fields_controller.js
@@ -1,10 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
+import { Sortable } from "sortablejs"
 
 export default class extends Controller {
   static targets = ["template", "container"]
   static values = { childIndex: String }
 
   connect() {
+    Sortable.create(this.containerTarget, {
+      onEnd: (event) => {
+        this.containerTarget.querySelectorAll('.nested-fields').forEach((item, index) => {
+          item.querySelector(`[data-position=${this.childIndexValue}]`).value = index + 1
+        })
+      }
+    })
   }
 
   add(event) {
@@ -18,7 +26,7 @@ export default class extends Controller {
     const item = event.currentTarget.closest('.nested-fields')
 
     if (item) {
-      item.querySelector('input[name*="_destroy"]').value = '1'
+      item.querySelector(`[data-destroy="${this.childIndexValue}"]`).value = '1'
       item.style.display = 'none'
     }
   }
